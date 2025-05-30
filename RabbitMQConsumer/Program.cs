123d95f72d7c9fcfb364e4c77e04749cdacb7eea
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-05-30 12:18:30 +0700

    pertemuan 13

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("task-queue", durable: true, exclusive: false, autoDelete: false);
+
+Console.WriteLine("[*] Waiting for messages.... To exit press CTRL+C");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] Received: {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000); // Simulate work by delaying based on the number of dots in the message
+    Console.WriteLine("[X] Done");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync(
+    queue: "task-queue",
+    autoAck: false, // Set to false to manually acknowledge messages,
+    consumer: consumer
+);
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("task-queue", durable: true, exclusive: false, autoDelete: false);
+
+var message = GetMessage(args);
+static string GetMessage(string[] args)
+{
+    return args.Length > 0 ? string.Join(" ", args) : "Hello World!";
+}
+var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+var properties = new BasicProperties
+{
+    Persistent = true, // Make the message persistent
+};
+await channel.BasicPublishAsync(
+    exchange: String.Empty,
+    routingKey: "task-queue",
+    mandatory: true,
+    basicProperties: properties,
+    body: body
+);
+
+Console.WriteLine($"[X] Sent: {message}");
+// Wait for user input before closing the application
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
